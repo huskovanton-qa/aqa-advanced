2d726ae43f6f43e8cff7a2dd8dcc0fe090189345
Huskov Anton <anton.huskov@appflame.com>
2026-04-21 19:54:30 +0300

    JsBasic commit

---
+++ jsBasics/task2.js
@@ -0,0 +1,9 @@
+const firstPerson = "Anton";
+const secondPerson = "Vlad";
+
+const greeting1 = "Hello, " + firstPerson + " and " + secondPerson;
+
+const greeting2 = `Hello, ${firstPerson} and ${secondPerson}`;
+
+console.log(greeting1);
+console.log(greeting2);
---
+++ jsBasics/task4.js
@@ -0,0 +1,19 @@
+//Task 4.1
+const radius1 = 5;
+const area = Math.PI * radius1 ** 2;
+
+console.log(area.toFixed(2));
+
+//Task 4.2
+const length = 10;
+const width = 5;
+const squareArea = length * width;
+
+console.log(squareArea.toFixed(2));
+
+//Task 4.3
+const radius2 = 5;
+const height = 10;
+const cylinderArea = Math.PI * radius2 ** 2 * height;
+
+console.log(cylinderArea.toFixed(2));
