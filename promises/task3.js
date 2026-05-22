7447580f7d5c9a58503d29561dc594c306b43e82
Huskov Anton <anton.huskov@appflame.com>
2026-05-22 14:42:34 +0300

    promises task commit

---
+++ promises/task3.js
@@ -0,0 +1,40 @@
+async function todos() {
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos/1'
+    );
+
+    const todo = await response.json();
+
+    return todo;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function users() {
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/users/1'
+    );
+
+    const user = await response.json();
+
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+const allPromises = Promise.all([todos(), users()]);
+
+allPromises.then((result) => {
+  console.log(result);
+});
+
+const racePromises = Promise.race([todos(), users()]);
+
+racePromises.then((result) => {
+  console.log(result);
+});
